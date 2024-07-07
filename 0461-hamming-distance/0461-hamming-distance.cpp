9417691e65184c532ff645f31da1acb96992a197
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-07 05:30:31 -0400

    Time: 3 ms (39.43%), Space: 7.1 MB (54.23%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result = x ^ y;
+        
+        int count = 0;
+        
+        while (result) {
+            count += result & 1;
+            result >>= 1;
+        }
+        
+        return count;
+    }
+};
