25e9d89837f2bf312ff4c6db87628c1c94f1ec02
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-10 00:51:01 -0400

    Time: 9 ms (68.48%), Space: 14.2 MB (18.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> output(2,0);
-        
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i+1; j < nums.size(); j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    output[0] = i;
-                    output[1] = j;
-                    return output;
-                }
+        std::unordered_map<int, int> numToIndex; // Hash map to store number and its index
+        std::vector<int> output(2, 0); // Result vector to store the indices
+
+        // Iterate over the nums array
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            
+            // Check if the complement is already in the hash map
+            if (numToIndex.find(complement) != numToIndex.end()) {
+                // If found, return the indices
+                output[0] = numToIndex[complement];
+                output[1] = i;
+                return output;
             }
+            
+            // If not found, store the current number and its index in the hash map
+            numToIndex[nums[i]] = i;
         }
-        
+
+        // In case no solution is found, though the problem guarantees exactly one solution
         return output;
     }
 };
