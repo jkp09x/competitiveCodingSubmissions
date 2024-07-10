8eda8bef092756e4abc6a7aa8263af423879fbc4
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-10 00:21:01 -0400

    Time: 2 ms (44.14%), Space: 10.2 MB (54.58%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {   
-        // add 1 to last element
-        digits[digits.size()-1] += 1;
+        int n = digits.size();
         
-        for (int i = digits.size() - 1; i >= 0; --i)
-        {
-            if (digits[i] > 9)
-            {
-                digits[i] = 0;
-                if (i > 0)
-                    digits[i-1] += 1;
-                if (i == 0)
-                {
-                    digits[i] = 1;
-                    digits.push_back(0);
-                }
+        // Start from the end of the vector
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] < 9) {
+                // No carry needed, just increment and return
+                digits[i]++;
+                return digits;
             }
+            // Set current digit to 0 and continue with the carry
+            digits[i] = 0;
         }
         
+        // If all digits were 9, we need an extra digit at the beginning
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
