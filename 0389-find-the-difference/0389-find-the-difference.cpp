420602d1fe98c3c414872b23161b77409e8cd47f
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-09 23:36:55 -0400

    Time: 4 ms (49.74%), Space: 7.8 MB (56.93%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        char result = 0;
+        
+        for (int i = 0; i < std::max(n,m); ++i)
+        {
+            if (i <= n)
+            {
+                result ^= s[i];
+            }
+            
+            if (i <= m)
+            {
+                result ^= t[i];
+            }
+        }
+
+        return result;
+    }
+};
