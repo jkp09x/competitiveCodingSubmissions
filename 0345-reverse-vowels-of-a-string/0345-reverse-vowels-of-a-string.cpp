ccb3db7dbb2350410cb6a363bb2bda4da5c541f2
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-13 02:09:42 -0400

    Time: 10 ms (29.54%), Space: 9.6 MB (32.89%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -8,22 +8,27 @@ public:
         // s consists of printable ASCII characters (which is a total of 128)
         bool asciiVowels[128] = {false};
         
-        // Set lower and uppercase vowels to true
+        // Set lower and uppercase vowels to true (everything else remains false)
         for (char c: "aeiouAEIOU")
         {
             asciiVowels[c] = true;
         }
         
-        while (lPtr < rPtr) {            
+        while (lPtr < rPtr) {
+            // find vowel from start of string
             while (lPtr < rPtr && !asciiVowels[s[lPtr]])
                 ++lPtr;
             
+            // Find vowel from end of string
             while (lPtr < rPtr && !asciiVowels[s[rPtr]])
                 --rPtr;
             
+            //Swap vowel positions (could use std::swap() as well)
             tempChar = s[lPtr]; 
             s[lPtr] = s[rPtr];
             s[rPtr] = tempChar;
+            
+            // Update Pointer positions
             ++lPtr;
             --rPtr;
         }
