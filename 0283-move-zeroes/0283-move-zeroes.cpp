67b22736fbfab1aec51c03d25fd2a3a7e52d538b
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-13 02:27:35 -0400

    Time: 20 ms (27.46%), Space: 22.4 MB (8.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int idx = 0;
+        
+        // Initialize vector of same size as nums with all zeros 
         vector<int> newVec(nums.size(), 0);
         
+        // Creates a new vector and only adds the non-zero elements to the front of the array        
         for (int num: nums) {
             if (num != 0) {
                 newVec[idx] = num;
                 ++idx;
             }
         }
+        
+        // assign new Vector back to nums
         nums = newVec;
     }
 };
