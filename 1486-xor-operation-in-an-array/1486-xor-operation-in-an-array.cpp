a5da0de27c4450ff2bf84505be205a67ff51bddb
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-13 04:54:19 -0400

    Time: 2 ms (50.14%), Space: 7.5 MB (12.00%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -4,13 +4,9 @@ public:
         int result = 0;
         
         for (int i = 0; i < n; ++i) {
-            if (i == 0)
-                result = start;
-            else
-                result ^= (start + 2 * i);
+            result ^= (start + 2 * i);
         }
 
         return result;
-
     }
 };
