a652ee9b69f79a5c0e1983deb9d966be56bb4490
Jigar Patel <jkp09x@users.noreply.github.com>
2024-07-13 04:50:24 -0400

    Time: 0 ms (100.00%), Space: 7.8 MB (6.79%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int result = 0;
+        vector<int> nums(n, 0);
+        
+        for (int i = 0; i < n; ++i) {
+            nums[i] = start + 2 * i;
+        }
+
+        if (n > 1){
+            result = nums[0] ^ nums[1];
+            for (int j = 2; j < nums.size(); ++j) {
+                result ^= nums[j];
+            }
+        }
+        else
+            return start;
+
+        return result;
+
+    }
+};
